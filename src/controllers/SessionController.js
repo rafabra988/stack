08d28846d83d1d3e53499581cc9652394bb711e7
rafabra988 <44869209+rafabra988@users.noreply.github.com>
2019-10-03 15:26:07 -0300

    Add files via upload

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,17 @@
+const User = require('../models/User');
+
+module.exports = {
+    async store(req, res){
+        const { email } = req.body;
+        // msm coisa q const email = req.body.email;
+
+        let user = await User.findOne({ email })
+
+        if(!user){
+            user = await User.create({ email });
+        }
+
+    
+        return res.json(user);
+    }
+};
